d8bb054c5d58edc6fdc78301fc8c41995ceb7ae7
davidreradovic <121823422+davidreradovic@users.noreply.github.com>
2026-05-11 14:04:44 +0200

    Delete backend directory

--- backend/src/main/java/com/example/backend/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-}
--- backend/src/main/java/com/example/backend/repository/SupervisorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Supervisor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupervisorRepository extends JpaRepository<Supervisor, Integer> {
-}
